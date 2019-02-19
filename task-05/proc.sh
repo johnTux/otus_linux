7e2a4296a9d9aceb7bd8b6d59cc2303af81f26a9
johnTux <johnTux@example.com>
2019-02-20 00:22:52 +0300

    add

--- task-05/proc.sh
+++ task-05/proc.sh
@@ -3,3 +3,24 @@
 PROC=/proc
 
 ls -a $PROC | grep -o '[0-9]*' | sort -n
+
+#!/bin/bash
+
+#PID=$PID
+#STAT=$STAT
+#COMMAND=$COMMAND
+#pidDir="/proc/pidDir"
+#awk 'BEGIN {print " PID STAT COMMAND\n-----------------------------------"}'
+#cat /proc/1131/status | awk '$1 == "Pid:" {print " " $2}' > ps
+#echo PID STAT COMMAND > ps
+#cat ps
+
+for proc in ls /proc/ | egrep "^[0-9]" | sort -n
+do
+if [ -d "/proc/$proc" ]; then
+cd /proc/$proc
+cat /proc/$proc/status | awk '$1 == "Pid:" {print " " $2}' > ps
+continue
+fi
+#echo $proc
+done
